1d66860cd569a35d07944243740cbe7adf9cd481
Shajeen Ahamed <2623563+shajeen@users.noreply.github.com>
2020-11-16 10:30:34 +0530

    Update env_linux.sh

--- env_linux.sh
+++ env_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install -y python3-pip git cmake gcc g++ xorg-dev libx11-xcb-dev libxcb-render0-dev libxcb-render-util0-dev \
 libxcb-xkb-dev libxcb-icccm4-dev libxcb-image0-dev  libxcb-keysyms1-dev libxcb-randr0-dev libxcb-shape0-dev libxcb-sync-dev \
-libxcb-xfixes0-dev libxcb-xinerama0-dev m4
+libxcb-xfixes0-dev libxcb-xinerama0-dev m4 libgl1-mesa-dev
 pip install conan mako
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan 
 
