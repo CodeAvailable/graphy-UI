b58dfe3ca453bd7b655c936a229aa1b5f42629fe
shajeen <shajeenahmed@gmail.com>
2020-11-16 09:07:58 +0530

    fix lib intergration, how works fine

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "graphyRestService.h"
 #include "graphyJsonModule.h"
+#include "graphyFileSelector.h"
+#include "graphyQMLEngine.h"
 
 int main()
 {
-    std::cout << "Module test: " << graphyRestService::test() << " " << graphyJsonModule::test() << std::endl;
+    std::cout << "Module test: " << graphyRestService::test() << " " << graphyJsonModule::test() <<
+                 " " << graphyFileSelector::test() << " " << graphyQMLEngine::test() <<std::endl;
 	return 0;
 }
